8d4f42d11f1483e2114f2bcfae11e9dc4a8a3d28
Todd Birchard <todd.birchard@wework.com>
2018-10-18 18:50:24 -0400

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+import beautifulsoup4
+import flask
+
+def scrape(request):
+    """Main request."""
+    if request.method == 'POST':
+        # Allows POST requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        request_json = request.get_json()
+        if request_json:
+            plantext = request_json['plain']
+            html = request_json['html']
+        else:
+            return html
