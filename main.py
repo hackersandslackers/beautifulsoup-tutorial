c7d48d85188eab59cdac56c536a9b3c93a1199af
Todd Birchard <toddbirchard@gmail.com>
2018-10-22 23:58:11 -0400

    progress

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import logging
 
-previews = []
 
 def scrape(request):
     """Main request."""
     if request.method == 'POST':
         # Allows POST requests from any origin with the Content-Type
         # header and caches preflight response for an 3600s
-        global previews
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST',
@@ -21,9 +18,10 @@ def scrape(request):
         ghost_url = request_json['url']
         r = requests.get(ghost_url)
         raw_html = r.text
-        print('raw_html = ', raw_html)
         html = BeautifulSoup(raw_html, 'html.parser')
-        links = html.find_all('.post-content a')
+        body = html.select('.post-content')
+        links = body.find_all('a')
+        previews = []
         for link in links:
             url = link.get('href')
             r2 = requests.get(url)
@@ -31,10 +29,9 @@ def scrape(request):
             link_preview = BeautifulSoup(link_html, 'html.parser')
             preview_dict = {
                 'title': link_preview.title.string,
-                'description': link_preview.find("meta",  property="og:description"),
+                'description': link_preview.find("meta",  name="description"),
                 'image': link_preview.find("meta",  property="og:image"),
                 'url': url
             }
-            logging.warn(preview_dict)
             previews.append(preview_dict)
-        return (previews, 200, headers)
+        return previews
