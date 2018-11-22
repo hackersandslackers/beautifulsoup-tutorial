d0b11e6ba62f8312242dd2a8017dd03484082b53
Todd Birchard <toddbirchard@gmail.com>
2018-11-22 02:25:25 -0500

    size down

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from meta import getMeta
 
 def scrape(request):
     """Scrape scheduled link previews."""
-    if request.method == 'POST':
+    if request.method == 'GET':
         # Allows POST requests from any origin with the Content-Type
         # header and caches preflight response for an 3600s
         target_url = request.args.get('url')
         headers = {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Methods': 'GET',
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600',
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
