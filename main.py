c78756d984cd4dbba5afa729995bf9787af649ee
toddbirchard <toddbirchard@gmail.com>
2018-11-22 00:26:02 -0500

    fixed conflicts

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def scrape(request):
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600'
         }
-        request_json = request.args.get('url')
+        request_json = request.get_json()
         html = request_json['html']
         headers.update({
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
