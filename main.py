cccbf16d450d489c1d2723ae1bcd2a66a4304edd
Todd Birchard <toddbirchard@gmail.com>
2018-11-22 02:20:05 -0500

    size down

--- main.py
+++ main.py
@@ -15,11 +15,9 @@ def scrape(request):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST',
             'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
+            'Access-Control-Max-Age': '3600',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
         }
-        headers.update({
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-        })
         links = getLinks(target_url, headers)
         previews = getMeta(links, headers)
         return make_response(previews, 200, headers)
