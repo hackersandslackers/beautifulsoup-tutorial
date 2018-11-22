e91d9b1aa3328830bc6ed101f670dea6d164fa1c
Todd Birchard <toddbirchard@gmail.com>
2018-11-22 02:17:35 -0500

    size down

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def scrape(request):
     if request.method == 'POST':
         # Allows POST requests from any origin with the Content-Type
         # header and caches preflight response for an 3600s
+        target_url = request.args.get('url')
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST',
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600'
         }
-        target_url = request.args.get('url')
         headers.update({
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
         })
