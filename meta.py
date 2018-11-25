d19c1f4da7084046250067d0d0faeba1cd3de6bc
Todd Birchard <toddbirchard@gmail.com>
2018-11-25 15:57:32 -0500

    findall

--- meta.py
+++ meta.py
@@ -6,10 +6,14 @@ from fallback import get_title, get_description, get_image, get_site_name
 def getMeta(links, headers):
     """Generate preview obj per link."""
     previews = []
+    exception_domains = ['Youtube', 'Medium' 'Github']
     for link in links:
         url = link.get('href')
         r = requests.get(url, headers=headers)
         embedded_link = BeautifulSoup(r.content, 'html.parser')
+        domain = get_site_name(embedded_link, url)
+        if domain in exception_domains:
+            print('WARNING:', domain)
         preview_dict = {
             'title': get_title(embedded_link),
             'description': get_description(embedded_link),
