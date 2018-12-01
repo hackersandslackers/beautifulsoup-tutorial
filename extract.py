23043bc3d46a49e07aaf9f8bacdbc6a1ffd45930
Todd Birchard <todd.birchard@wework.com>
2018-11-30 21:01:47 -0500

    Update extract.py

--- extract.py
+++ extract.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def getLinks(html, headers):
+def get_links(html, headers):
     """Extract links from url."""
     r = requests.get(html, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-    links = soup.select('.post-content p > a')
+    links = soup.select('.post-content').find_all('a').get('href')
     return links
