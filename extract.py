6269870c50d764e0ba788946a427738a9ca778f2
Todd Birchard <toddbirchard@gmail.com>
2018-12-01 00:35:21 -0500

    yo ush

--- extract.py
+++ extract.py
@@ -6,7 +6,7 @@ def get_links(html, headers):
     """Extract links from url."""
     r = requests.get(html, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-    links = soup.select('.post-content').find_all('a')
+    links = soup.select('.post-content a')
     link_arr = []
     for link in links:
         link_arr.append(link.get('href'))
