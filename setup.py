1c3dd46173c7d0478ca7a623305125c71af4f5a9
Todd Birchard <toddbirchard@gmail.com>
2018-12-02 03:01:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(
-    # ...,
+with open("README", 'r') as f:
+    long_description = f.read()
+
+    name='Lynx',
+    version='1.0',
+    description='Generates a JSON object containing link preview information by passing a URL parameter.',
+    long_description=long_description,
+    author='Todd Birchard',
+    author_email='toddbirchard@gmail.com',
+    url="https://github.com/toddbirchard/Link-Preview-API",
     tests_require=["pytest"],
     cmdclass={"pytest": PyTest},
 )
