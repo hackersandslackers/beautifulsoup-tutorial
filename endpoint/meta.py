7e70a6b837ecac798013dd3a8dd650bc9f71e914
Todd Birchard <toddbirchard@gmail.com>
2018-12-16 21:28:15 -0500

    updated logic for single link

--- endpoint/meta.py
+++ endpoint/meta.py
@@ -3,23 +3,19 @@ from bs4 import BeautifulSoup
 from fallback import get_title, get_description, get_image, get_site_name
 
 
-def get_meta(links, headers):
+def get_meta(link, headers):
     """Generate preview obj per link."""
-    previews = []
     exception_domains = ['Youtube', 'Medium' 'Github']
-    for link in links:
-        url = link.get('href')
-        r = requests.get(url, headers=headers)
-        embedded_link = BeautifulSoup(r.content, 'html.parser')
-        domain = get_site_name(embedded_link, url)
-        if domain in exception_domains:
-            print('WARNING:', domain)
-        preview_dict = {
-            'title': get_title(embedded_link),
-            'description': get_description(embedded_link),
-            'image': get_image(embedded_link),
-            'sitename': get_site_name(embedded_link, url),
-            'url': url
-            }
-        previews.append(preview_dict)
-    return previews
+    r = requests.get(link, headers=headers)
+    embedded_link = BeautifulSoup(r.content, 'html.parser')
+    domain = get_site_name(embedded_link, link)
+    if domain in exception_domains:
+        print('WARNING:', domain)
+    preview_dict = {
+        'title': get_title(embedded_link),
+        'description': get_description(embedded_link),
+        'image': get_image(embedded_link),
+        'sitename': get_site_name(embedded_link, link),
+        'url': link
+        }
+    return preview_dict
