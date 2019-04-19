8bdc7a2d59aa557c96d5172ee50eb2012694c781
Todd Birchard <toddbirchard@gmail.com>
2019-04-18 22:23:22 -0400

    readded headers to request response

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ def scrape(request):
     3. Convert metadata to JSON object.
     4. Return response.
     """
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
     target_url = request.args.get('url')
     previews = get_meta(target_url)
     response_body = json.dumps(previews)
-    return make_response(str(response_body), 200)
+    return make_response(str(response_body), 200, headers)
