465230320a390eba4d61e71a074e48b6e4834c8b
Todd Birchard <toddbirchard@gmail.com>
2019-04-19 21:47:35 -0400

    refactored fetch.py

--- fetch.py
+++ fetch.py
@@ -2,35 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_meta(link):
-    """Generate preview obj per link.
-
-    1. Determine title of target url.
-    2. Create description blurb of target url.
-    3. Find suitable image for target url.
-    4. Determine the top-level site name of target url.
-    5. Create dict of fetched metadata.
-    6. Return result.
-    """
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
-    r = requests.get(link, headers=headers)
-    embedded_link = BeautifulSoup(r.content, 'html.parser')
-    preview_dict = {
-        'title': get_title(embedded_link),
-        'description': get_description(embedded_link),
-        'image': get_image(embedded_link),
-        'sitename': get_site_name(embedded_link, link),
-        'url': link
-        }
-    return preview_dict
-
-
 def get_site_name(link, url):
     """Attempt to get the site's base name.
 
@@ -117,3 +88,32 @@ def site_exceptions(link, url):
     exception_domains = ['Youtube', 'Medium' 'Github']
     if domain in exception_domains:
         print('WARNING:', domain)
+
+
+def get_meta(link):
+    """Generate preview obj per link.
+
+    1. Determine title of target url.
+    2. Create description blurb of target url.
+    3. Find suitable image for target url.
+    4. Determine the top-level site name of target url.
+    5. Create dict of fetched metadata.
+    6. Return result.
+    """
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+    r = requests.get(link, headers=headers)
+    embedded_link = BeautifulSoup(r.content, 'html.parser')
+    preview_dict = {
+        'title': get_title(embedded_link),
+        'description': get_description(embedded_link),
+        'image': get_image(embedded_link),
+        'sitename': get_site_name(embedded_link, link),
+        'url': link
+        }
+    return preview_dict
