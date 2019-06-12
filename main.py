8b87d451d671f0a9ef50ca64431e827b5dc46ecc
Todd Birchard <toddbirchard@gmail.com>
2019-06-12 01:22:44 -0400

    CORS headers

--- main.py
+++ main.py
@@ -10,19 +10,6 @@ def scrape():
     3. Convert metadata to JSON object.
     4. Return response.
     """
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-
-        }
-        return ('', 204, headers)
     headers = {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
@@ -34,4 +21,4 @@ def scrape():
     response = jsonify(previews)
     response.headers.set('Access-Control-Allow-Origin', '*')
     response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
-    return str(response)
+    return response
