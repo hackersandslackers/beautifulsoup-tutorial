b2a7f72d21bb854c84979c8e819992eac24adc3f
Todd Birchard <toddbirchard@gmail.com>
2019-06-12 01:27:59 -0400

    CORS headers

--- main.py
+++ main.py
@@ -2,20 +2,8 @@ from flask import Flask, make_response, request, jsonify
 from fetch import get_meta
 
 
-def scrape():
-    """Scrape scheduled link previews.
-
-    1. Set headers of fetch request.
-    2. Call get_meta constructor.
-    3. Convert metadata to JSON object.
-    4. Return response.
-    """
-    headers = {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-        }
+def scrape(request):
+    """Scrape URL provided in querystring param to create preview."""
     target_url = request.args.get('url')
     previews = get_meta(target_url)
     response = jsonify(previews)
